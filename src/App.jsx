4bf2e6fe0d36ab6dbea62d13ed3de8a55e463e32
kamalrajms <kamalrajindtn@gmail.com>
2025-10-27 11:29:29 +0530

    intro

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Greeting from "./Greeting";
+
+export default function () {
+  return (
+    <>
+      <h1>React class</h1>
+      <Greeting />
+      <Greeting />
+      <h2>fbehe</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum beatae
+        necessitatibus, magnam, dicta quasi quod illum consequatur odio
+        similique in, optio esse! Consequatur deleniti laboriosam deserunt ab
+        sint corrupti est.
+      </p>
+    </>
+  );
+}
